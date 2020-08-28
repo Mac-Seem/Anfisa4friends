ce63fe2b2f948a980ef19aea5f7297d90f28a5a7
Maxim Plotnikov <maximsign@gmail.com>
2020-08-27 20:32:02 -0700

    gitignore mod

--- anfisa/anfisa/urls.py
+++ anfisa/anfisa/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-#dhdkj
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path ('', include('homepage.urls'))
